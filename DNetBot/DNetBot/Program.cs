f61d9c1f1294860a745d47b74a745717d9d1c122
Glenn Prince <glenn_prince@outlook.com>
2019-08-17 07:10:50 +1000

    Added Basic Background Services

--- DNetBot/DNetBot/Program.cs
+++ DNetBot/DNetBot/Program.cs
@@ -1,12 +1,44 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace DNetBot
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var host = new HostBuilder()
+                .ConfigureHostConfiguration(configHost =>
+                {
+                    configHost.SetBasePath(Directory.GetCurrentDirectory());
+                    configHost.AddJsonFile("Config/hostsettings.json", optional: true);
+                    configHost.AddEnvironmentVariables(prefix: "BOT_");
+                    configHost.AddCommandLine(args);
+                })
+                .ConfigureAppConfiguration((hostContext, configApp) =>
+                {
+                    configApp.AddJsonFile("Config/appsettings.json", optional: true);
+                    configApp.AddJsonFile(
+                        $"Config/appsettings.{hostContext.HostingEnvironment.EnvironmentName}.json",
+                        optional: true);
+                    configApp.AddEnvironmentVariables(prefix: "BOT_");
+                    configApp.AddCommandLine(args);
+                })
+                
+                .ConfigureLogging((hostContext, configLogging) =>
+                {
+                    configLogging.AddConfiguration(hostContext.Configuration.GetSection("Logging"));
+                    configLogging.AddConsole();
+                    configLogging.AddDebug();
+                })
+                .UseConsoleLifetime()
+                .Build();
+
+            await host.RunAsync();
         }
     }
 }
